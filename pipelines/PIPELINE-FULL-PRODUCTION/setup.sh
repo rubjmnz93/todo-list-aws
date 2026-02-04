43367f73c95705be4a6f5f1c2f9ea5cfd47547bc
Ruben Jimenez Andreu <rubenjmnz93@gmail.com>
2026-02-04 20:30:50 +0100

    Fixed setup environment

--- pipelines/PIPELINE-FULL-PRODUCTION/setup.sh
+++ pipelines/PIPELINE-FULL-PRODUCTION/setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 set -x
-python3.7 -m venv todo-list-aws
-source todo-list-aws/bin/activate
+python3 -m venv .venv
+source .venv/bin/activate
 python -m pip install --upgrade pip
 python -m pip install awscli
 python -m pip install aws-sam-cli
